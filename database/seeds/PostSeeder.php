b5315d1591d13331499f37d522496f3a3df7b722
Fede <chiellofederico@gmail.com>
2021-03-26 12:27:03 +0100

    faccio il ciclo for per i dati faker

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Post;
+
 
 class PostSeeder extends Seeder
 {
@@ -9,8 +13,13 @@ class PostSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++){
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(5);
+            $newPost->content = $faker->text(300);
+            $newPost->save(); 
+        }
     }
 }
