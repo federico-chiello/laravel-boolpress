5130a17ae4624345bf2e1a4d31842b525f1833f1
Fede <chiellofederico@gmail.com>
2021-03-27 11:30:51 +0100

    metto un'interfaccia alla pagina

--- resources/views/guests/post/index.blade.php
+++ resources/views/guests/post/index.blade.php
@@ -4,4 +4,16 @@
     
 @section('content')
 <h1>Elenco dei post</h1>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+          Featured
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+</div>
 @endsection
