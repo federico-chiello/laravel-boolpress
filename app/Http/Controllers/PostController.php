90cf6df49415bd21a48401af17da0f7ff774306d
Fede <chiellofederico@gmail.com>
2021-03-28 11:42:36 +0200

    passo i dati dei posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function index(){
-        return view('guests.post.index'); 
+        $posts = Post::all();
+        $data = [
+            'post' => $posts
+        ];
+        return view('guests.post.index', $data); 
     }
 }
