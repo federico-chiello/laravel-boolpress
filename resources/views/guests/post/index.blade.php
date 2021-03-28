07365f1b282c92a632c1bc9192cf0f48b504b09c
Fede <chiellofederico@gmail.com>
2021-03-28 11:50:37 +0200

    uso il foreach per caricare i dati

--- resources/views/guests/post/index.blade.php
+++ resources/views/guests/post/index.blade.php
@@ -5,15 +5,16 @@
 @section('content')
 <h1>Elenco dei post</h1>
 <div class="container">
+  @foreach ($post as $posts)
     <div class="card">
-        <div class="card-header">
-          Featured
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+      <div class="card-header">
+        {{ $posts->title }}
       </div>
+      <div class="card-body">
+        <p class="card-text">iv class="card-header">{{ $posts->content }}</p>
+        <a href="#" class="btn btn-primary">Dettagli</a>
+      </div>
+    </div>
+  @endforeach
 </div>
 @endsection
