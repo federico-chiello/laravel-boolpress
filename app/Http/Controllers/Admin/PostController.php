af57e8fc241173d287e29b8f9b76906f2d8de067
Fede <chiellofederico@gmail.com>
2021-03-28 19:27:49 +0200

    faccio l'update

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -92,9 +92,11 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $data = $request->all();
+        $post->update($data);
+        return redirect()->route('post.show', $post);
     }
 
     /**
