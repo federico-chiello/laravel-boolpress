42c4d59352c60a79b7ef9c539aa5d0fa29744a22
Fede <chiellofederico@gmail.com>
2021-03-29 15:47:59 +0200

    creo il layout della pagina

--- resources/views/guests/contacts.blade.php
+++ resources/views/guests/contacts.blade.php
@@ -1,1 +1,21 @@
 @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form>
+        <div class="form-group">
+            <label for="name">Nome</label>
+            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name">
+          </div>
+        <div class="form-group">
+          <label for="email">Indirizzo Email</label>
+          <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
+        </div>
+        <div class="form-group">
+            <label for="message">Messaggio</label>
+            <textarea class="form-control" id="message" rows="3" name="message"></textarea>
+          </div>
+        <button type="submit" class="btn btn-primary">Salva</button>
+    </form>
+</div>
+@endsection
