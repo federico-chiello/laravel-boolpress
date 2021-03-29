d25beca32e7999edef0ca49a481dba1a62acbaf6
Fede <chiellofederico@gmail.com>
2021-03-29 16:26:52 +0200

    faccio l'istanza e uso il metodo fill in contactsSent

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Lead;
 
 class HomeController extends Controller
 {
@@ -25,5 +27,9 @@ class HomeController extends Controller
     public function contactsSent(Request $request)
     {
         $data = $request->all();
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+        return redirect()->route('guests.contacts');
     }
 }
