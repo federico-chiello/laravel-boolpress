fa3a577b25854b748ec94fc832e06d15fd712e9b
Fede <chiellofederico@gmail.com>
2021-04-02 16:17:45 +0200

    stampo i dati dell'utente

--- resources/views/admin/user/profile.blade.php
+++ resources/views/admin/user/profile.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati Utente</h1>
-    
+<div class="container">
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+          Dati utente
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">{{ Auth::user()->name }}</li>
+          <li class="list-group-item">{{ Auth::user()->email }}</li>
+          <li class="list-group-item">Token</li>
+        </ul>
+    </div>
+</div>
 @endsection
 
 
