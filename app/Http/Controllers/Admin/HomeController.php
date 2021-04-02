c6f9a3fd41bf46b16d34e686311d041a9f6fa7cf
Fede <chiellofederico@gmail.com>
2021-04-02 18:21:42 +0200

    funzione generaToken

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -15,5 +17,14 @@ class HomeController extends Controller
     public function profile()
     {
         return view('admin.user.profile');
+    }
+
+    public function generaToken()
+    {
+        $api_token = Str::random(80);
+        $user = Auth::user();
+        $user->api_token = $api_token;
+        $user->save();
+        return redirect()->route('profile');
     }
 }
