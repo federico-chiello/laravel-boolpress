2b0b86edb6f5e757d172f2f5fd32f5879ebd2225
Fede <chiellofederico@gmail.com>
2021-04-02 18:53:03 +0200

    aggiungo il middleware

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', 'Api\PostController@index')->name('guests.posts.index');
+Route::get('/posts', 'Api\PostController@index')->middleware('api_token');
